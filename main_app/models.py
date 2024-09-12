4abd336c82b1363d21e05864e172faebb39a47ae
daroo22 <rodneysomto@gmail.com>
2024-09-12 11:49:00 -0400

    progress

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from datetime import date
 
-# A tuple of 2-tuples
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
@@ -14,20 +14,24 @@ class Dog(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
 
+  # new code below
     def __str__(self):
-        return self.name
-
-# Add new Feeding model below Dog model
+      return self.name
+    
+    def fed_for_today(self):
+      return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
 class Feeding(models.Model):
-    date = models.DateField()
-    meal = models.CharField(
-        max_length=1,
-        # Add the 'choices' field option
-        choices=MEALS,
-        # Set the default value for meal to be 'B'
-        default=MEALS[0][0]
-    )
-    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+	      choices=MEALS,
+	      default=MEALS[0][0]
+  )
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
   
-    def __str__(self):
-        return f"{self.get_meal_display()} on {self.date}"
+  class Meta:
+    ordering = ['-date']
